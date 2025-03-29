390b58bc8ef552ad874da7e9bfaedc312b61ca81
eastba <157190776+eastba@users.noreply.github.com>
2025-03-29 15:33:07 +0800

    Update system_info.py
    打印信息头修改为当天日期，纯粹是为了方便pushpush的通知好看

--- utils/system_info.py
+++ utils/system_info.py
@@ -7,21 +7,23 @@ LastEditTime: 2025-03-28 16:00:51
 
 import platform
 from urllib.request import getproxies
+from datetime import datetime  # 新增导入
 
-from utils.__version__ import VERSION
 from utils.logger import log
 
 
 def print_info():
     """打印系统信息"""
+    current_date = datetime.now().strftime("%Y-%m-%d")  # 获取当前日期并格式化
+    log.info(current_date)  # 替换为日期
     log.info("---------- 系统信息 -------------")
     system_info()
-    log.info("---------- 脚本日志 -------------")
+    log.info("---------- 运行日志 -------------")
 
 
 def system_info():
     """系统信息"""
-    log.info(show_info("操作系统平台", platform.platform()))
+    log.info(show_info("系统平台", platform.platform()))
     log.info(
         show_info(
             "Python 版本",
